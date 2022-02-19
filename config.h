4e591961fd1c1b8b6d16c5b1cefcd4d054395c0c
Madeleine <32415621+SpyHoodle@users.noreply.github.com>
2022-02-19 22:22:46 +0000

    Add colours and change scripts

--- config.h
+++ config.h
@@ -1,10 +1,12 @@
-#define CMDLENGTH 50
-#define DELIMITER " | "
+#define CMDLENGTH 100
+#define DELIMITER "^c#D6DEEB^ | "
+#include "/home/spy/cols/dwmblocks.h"
 
 const Block blocks[] = {
-	BLOCK("sb-volume",  0, 5)
-	BLOCK("sb-disk",    5, 20)
-	BLOCK("sb-memory",  5, 18)
-	BLOCK("sb-cpu",     5, 19)
-	BLOCK("sb-clock",   1, 21)
+	BLOCK("echo ^c#56B6C2^$(sb-volume)",           0,    1)
+	BLOCK("echo ^c#C678DD^$(sb-disk)",             1,    2)
+	BLOCK("echo ^c#D19A66^$(sb-memory)",           1,    3)
+	BLOCK("echo ^c#61AFEF^$(sb-cpu) $(sb-temp)",   5,    4)
+	BLOCK("echo ^c#98C379^$(sb-kernel)",           5,    5)
+	BLOCK("echo ^c#E06C75^$(sb-clock)",            1,    6)
 };
