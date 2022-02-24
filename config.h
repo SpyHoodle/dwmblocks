ebd9e845ebb92cd3c83fa0b2adee344860a54a92
spy <spyhoodle@gmail.com>
2022-02-24 01:23:07 +0000

    some minor changes

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 #define CMDLENGTH 100
-#define DELIMITER "^c#D6DEEB^ | "
+#define DELIMITER " "
 #include "/home/spy/cols/dwmblocks.h"
 
 const Block blocks[] = {
-	BLOCK("echo ^c#56B6C2^$(sb-volume)",           0,    1)
-	BLOCK("echo ^c#C678DD^$(sb-disk)",             1,    2)
-	BLOCK("echo ^c#D19A66^$(sb-memory)",           1,    3)
-	BLOCK("echo ^c#61AFEF^$(sb-cpu) $(sb-temp)",   5,    4)
-	BLOCK("echo ^c#98C379^$(sb-kernel)",           5,    5)
-	BLOCK("echo ^c#E06C75^$(sb-clock)",            1,    6)
+	BLOCK("echo ^c#D6DEEB^[ ^c#56B6C2^$(sb-volume)^c#D6DEEB^ ]",           0,    1)
+	BLOCK("echo ^c#D6DEEB^[ ^c#C678DD^$(sb-disk)^c#D6DEEB^ ]",             1,    2)
+	BLOCK("echo ^c#D6DEEB^[ ^c#D19A66^$(sb-memory)^c#D6DEEB^ ]",           1,    3)
+	BLOCK("echo ^c#D6DEEB^[ ^c#61AFEF^$(sb-cpu) $(sb-temp)^c#D6DEEB^ ]",   5,    4)
+	BLOCK("echo ^c#D6DEEB^[ ^c#98C379^$(sb-kernel)^c#D6DEEB^ ]",           5,    5)
+	BLOCK("echo ^c#D6DEEB^[ ^c#E06C75^$(sb-clock)^c#D6DEEB^ ]",            1,    6)
 };
